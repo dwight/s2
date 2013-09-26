1009a8e0b5d19adca359a4ce55ca6785d278c839
Dwight <dwight@10gen.com>
2013-09-26 13:47:42 -0400

    checkpoint

--- s2/storage2.cpp
+++ s2/storage2.cpp
@@ -10,7 +10,15 @@ using namespace std;
 */
 
 namespace mongo { 
-    
+
+    // stubs
+
+    class DiskLoc { 
+        unsigned long long x;
+    };
+
+    // ---
+
     class NS;
     class DiskLoc;
     class DB;
@@ -27,7 +35,28 @@ namespace mongo {
         void remove(DiskLoc extLoc);
     };
 
+    class VLoc;
+    class ScopedRecord { 
+        ScopedRecord(const DB&, VLoc);
+        ScopedRecord(const DB&, DiskLoc);
+        bool fitsInRecord(unsigned size) const;
+        // unsigned recSize() const;
+        char* data(); // e.g. bson or btree bucket
+        const char* data() const;
+        VLoc loc() const;
+        void grow(unsigned newSize);
+    };
+    class RecordInterface { 
+        //ScopedRecord alloc(unsigned sizeMin);
+        ScopedRecord insert(const NS&, const char *data, unsigned len);
+        void remove(VLoc loc);
+    };
+
     namespace s2 { 
+        class NSDetails { 
+            DiskLoc firstExtent;
+            DiskLoc lastExtent;
+        };
     }
 
 }
